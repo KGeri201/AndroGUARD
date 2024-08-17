0c2dfb8ddd49f5ad075d57fc97bd3a1cc1f01ab0
KGeri201 <36195747+kgerg@users.noreply.github.com>
2024-08-17 21:42:24 +0200

    updated validation method

--- code/validation/utils.py
+++ code/validation/utils.py
@@ -2,19 +2,21 @@ import os
 import numpy as np
 import pandas as pd
 
-import sys
-
 SENSORS = {
     "ACG.csv": ('x', 'y', 'z'),
     "GYRO.csv": ('x', 'y', 'z')
 }
 
-def readCSVFile(file: str, nrows: int = 12000) -> np.ndarray:  
+NR_FEATURES = len([i for sub in SENSORS.values() for i in sub])
+NR_MEASUREMENTS = 12000
+
+def readCSVFile(file: str, nrows: int = NR_MEASUREMENTS) -> np.ndarray:  
     """
     Reads predefined columns of CSV files, which we are interested in.
  
     Args:
         file (str): CSV file to be parsed.
+        nrows (int): Number of rows to be read from the CSV.
  
     Returns:
         np.ndarray: numpy array of the parsed columns.
@@ -23,13 +25,14 @@ def readCSVFile(file: str, nrows: int = 12000) -> np.ndarray:
     if os.path.split(file)[1] not in SENSORS: raise Exception('CSV is not in list.')
     return pd.read_csv(file, delimiter=';', usecols=SENSORS[os.path.split(file)[1]], nrows=nrows).to_numpy()
 
-def combineCSVs(path: str, output: str = None) -> np.ndarray:
+def combineCSVs(path: str, output: str = None, nrows: int = NR_MEASUREMENTS) -> np.ndarray:
     """
     Combines CSV files contained in a folder.
  
     Args:
         path (str): Folder containing the CSV files.
         output (str): CSV files the data is written to. If it is none, no data will be saved. Defaults to None.
+        nrows (int): Number of rows to be read from the CSV.
  
     Returns:
         np.ndarray: numpy array of the combined CSVs without the header.
@@ -38,19 +41,20 @@ def combineCSVs(path: str, output: str = None) -> np.ndarray:
     files = path if path.endswith('.csv') else [file for file in os.listdir(path) if file.endswith('.csv') and file in SENSORS.keys()]
     if not files: raise Exception('No valid CSV file was found at the given location.')
     for filename in files:
-        tmp = readCSVFile(path if path.endswith('.csv') else os.path.join(path, filename))
+        tmp = readCSVFile(path if path.endswith('.csv') else os.path.join(path, filename), nrows=nrows)
         features = np.hstack((features, tmp)) if features is not None else tmp
         if path.endswith('.csv'): break
     if output is not None:
         np.savetxt(output, features, delimiter=',', fmt='%s')
     return features
 
-def loadData(path: str) -> tuple[np.ndarray, np.ndarray]:
+def loadData(path: str, nr_measurements: int = NR_MEASUREMENTS) -> tuple[np.ndarray, np.ndarray]:
     """
     Parses CSV files contained in any subfolder of the given directory.
  
     Args:
         path (str): Folder containing the directories with the CSV files.
+        nr_measurements (int): Number of measuremnts to include int the dataset for each CSV.
  
     Returns:
         tuple[np.ndarray, np.ndarray]: tuple of features, parsed and combined data from the CSVs and the targets.
@@ -60,9 +64,10 @@ def loadData(path: str) -> tuple[np.ndarray, np.ndarray]:
     files = path if path.endswith('.csv') else [file for file in os.listdir(path) if os.path.isdir(os.path.join(path, file))]
     if not files: raise Exception('No valid folders were found at the given location.')
     for target, filename in enumerate(files):
-        tmp = combineCSVs(path if path.endswith('.csv') else os.path.join(path, filename))
+        tmp = combineCSVs(path if path.endswith('.csv') else os.path.join(path, filename), nrows=nr_measurements)
+        if tmp.shape[1] != NR_FEATURES : tmp = np.resize(tmp, (tmp.shape[0], NR_FEATURES))
         features = np.vstack((features, tmp)) if features is not None else tmp
-        targets = np.concatenate((targets, np.full((tmp.shape[0], 1), target)), axis=None)
+        targets = np.concatenate((targets, np.full((tmp.shape[0], 1), filename)), axis=None)
         if path.endswith('.csv'): break
 
     return features, targets
--- code/validation/validate.py
+++ code/validation/validate.py
@@ -1,30 +1,75 @@
 import argparse
+
 from utils import loadData
 
-from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
 from sklearn.neighbors import KNeighborsClassifier
-from sklearn.metrics import accuracy_score
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import GridSearchCV, train_test_split
+from sklearn.metrics import accuracy_score, classification_report
 
-def classify(path: str) -> None:
+def classify(path: str, 
+            classifier: str = 'knn', 
+            gridsearch: bool = True
+            ) -> KNeighborsClassifier | RandomForestClassifier:
     """
     Loads the data of the given directory and trains a KNN classification algorithm, then tests the accuracy. 
  
     Args:
         path (str): Folder containing the directories with the CSV files.
+        classifier (str): Selected classifier method.
+        gridsearch (bool): Should the parameters for the model be optimised via gridsearch or use default ones.
  
     Returns:
-        None
+        Classifier: model trained on the dataset.
     """
     X, y = loadData(path)
 
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=42)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
 
-    knn = KNeighborsClassifier()
-    knn.fit(X_train, y_train)
+    scaler = StandardScaler()
+    X_train_scaled = scaler.fit_transform(X_train)
+    X_test_scaled = scaler.transform(X_test)
 
-    y_pred = knn.predict(X_test)
+    model = None
+    param_grid = None
 
-    print(f"Accuracy: {accuracy_score(y_test, y_pred):.2f}")
+    if classifier == 'knn':
+        param_grid = {
+            'n_neighbors': [1, 2, 5],
+            'weights': ['uniform', 'distance'],
+            'algorithm': ['auto', 'ball_tree', 'kd_tree', 'brute']
+        }
+        model = KNeighborsClassifier(algorithm = 'auto', n_neighbors = 1, weights = 'uniform')
+    elif classifier == 'rf':
+        param_grid = {
+            'n_estimators': [200, 250, 300], 
+            'max_depth': [30, 40, 50]
+        }
+        model = RandomForestClassifier(n_estimators = 250, max_depth = 40)
+
+    if gridsearch:    
+        if param_grid is None:
+            raise ValueError("Parameters for GridSearch are not set.")
+
+        grid_search = GridSearchCV(model, param_grid, cv=5, scoring='accuracy')
+        grid_search.fit(X_train_scaled, y_train)
+        best_model = grid_search.best_estimator_
+    else:
+        best_model = model
+
+    if best_model is None:
+        raise ValueError("Model is not set.")
+
+    y_pred = best_model.predict(X_test_scaled)
+
+    print("Accuracy:", accuracy_score(y_test, y_pred))
+    print("Best Parameters:", best_model.get_params())
+    print("")
+    print(classification_report(y_test, y_pred))
+
+    return best_model
+
 
 if __name__ == "__main__":   
     parser = argparse.ArgumentParser()
@@ -33,7 +78,15 @@ if __name__ == "__main__":
                         type=str,
                         help='Input file or folder',
                         required=True)
+    
+    parser.add_argument('-c', '--classifier',
+                    dest='classifier',
+                    type=str,
+                    default = 'knn',
+                    choices = ['knn', 'rf'],
+                    help='Input file or folder')
 
     args = parser.parse_args()
 
-    classify(args.input)
+    classify(args.input, args.classifier)
+    
